e5acae170439083c77e3deac68fa96ab230b2ebb
Andrew Matelis <matelisa@gonzaga.org>
2026-01-08 22:13:52 -0500

    Fixed UI and Layout and Logic

---
+++ Evaluation/__init__.py
@@ -0,0 +1,11 @@
+from .metrics import compute_metrics, calibration_curve_plot
+from .visualization import plot_roc_curve, plot_feature_importance
+from .reporting import generate_evaluation_report
+
+__all__ = [
+    "compute_metrics",
+    "calibration_curve_plot",
+    "plot_roc_curve",
+    "plot_feature_importance",
+    "generate_evaluation_report",
+]
